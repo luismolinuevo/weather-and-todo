eeeebdbdadde41621119125c48294be76699504c
Luis Molinuevo <luismolinuevo0@gmail.com>
2023-06-25 03:55:39 -0400

    fetchForecast function working, tested, and giving me what I need

--- client/my-react-app/src/loaders/weather.js
+++ client/my-react-app/src/loaders/weather.js
@@ -1,10 +1,54 @@
 import axios from 'axios';
 
-export async function weather() {
+export async function fetchForecast() {
   try {
     const response = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=New%20York,US&appid=${import.meta.env.VITE_API_KEY}`);
-    const data = response.data;
-    return data;
+    const forecastData = response.data.list;
+    console.log(forecastData)
+    const forecast = {};
+
+    //this code will allow me get the highest temp, lowest temp, date, first icon, and day as a string per day
+    forecastData.forEach((data) => {
+        const date = new Date(data.dt * 1000);
+        const maxTemp = data.main.temp_max;
+        const minTemp = data.main.temp_min;
+        const icon = data.weather[0].icon;
+  
+        const dayOfWeek = new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(date);
+  
+        const formattedDate = date.toLocaleDateString();
+        const fullDate = formattedDate;
+  
+        if (fullDate in forecast) {
+          if (maxTemp > forecast[fullDate].maxTemp) {
+            forecast[fullDate].maxTemp = maxTemp;
+          }
+          if (minTemp < forecast[fullDate].minTemp) {
+            forecast[fullDate].minTemp = minTemp;
+          }
+        } else {
+          forecast[fullDate] = {
+            dayOfWeek,
+            maxTemp,
+            minTemp,
+            icon,
+          };
+        }
+      });
+  
+      //format the object to give me what I need
+      const dailyForecast = Object.entries(forecast).map(([date, { dayOfWeek, maxTemp, minTemp, icon }]) => ({
+        date,
+        dayOfWeek,
+        maxTemp,
+        minTemp,
+        icon,
+      }));
+  
+
+    return dailyForecast;
+
+
   } catch (error) {
     console.error('Error fetching data:', error);
     throw new Error('Failed to fetch data');
--- client/my-react-app/src/pages/HomePage.jsx
+++ client/my-react-app/src/pages/HomePage.jsx
@@ -1,6 +1,22 @@
-// import {useState, useEffect } from 'react'
+import {useState, useEffect } from 'react'
+import { fetchForecast } from '../loaders/weather';
 
 export default function HomePage() {
+    const [forecast, setForecast] = useState([]);
+
+    useEffect(() => {
+        const getWeather = async () => {
+            try {
+                const fetchData = await fetchForecast();
+                setForecast(fetchData);
+                console.log(fetchData)
+            } catch(error) {
+                console.log("There has been a issue fetching the forecast")
+            }
+        }
+
+        getWeather();
+    }, [])
   return (
     <div>
 
