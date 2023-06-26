ad2754025441d796fa678e51b78d33b2a8fe74fb
Luis Molinuevo <luismolinuevo0@gmail.com>
2023-06-26 04:52:03 -0400

    get all todos creating and working

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -3,6 +3,21 @@ const { Todo } = require('../models');
 
 const router = express.Router();
 
+//get all todos
+router.get("/", async (req, res) => {
+    try {
+        const fetchTodos = await Todo.findAll();
+
+        res.status(200).json({
+            success: true,
+            fetchTodos
+        })
+    } catch(err) {
+        console.log("Error fetching todos" + err)
+    }
+})
+
+//creates a new post
 router.post("/", async (req, res) => {
     try {
         const {description} = req.body;
