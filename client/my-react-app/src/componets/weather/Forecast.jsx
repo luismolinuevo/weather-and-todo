4e6312129d009c2a0a60c0226f4903c77f405a7f
Luis Molinuevo <luismolinuevo0@gmail.com>
2023-06-25 23:11:02 -0400

    five day forecast styles complete

--- client/my-react-app/src/componets/weather/Forecast.jsx
+++ client/my-react-app/src/componets/weather/Forecast.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { fetchForecast } from "../../loaders/weather";
+import { BiUpArrowAlt, BiDownArrowAlt} from "react-icons/bi"
 
 export default function Forecast() {
   const [forecast, setForecast] = useState([]);
@@ -9,6 +10,7 @@ export default function Forecast() {
       try {
         const fetchData = await fetchForecast();
         setForecast(fetchData);
+
         console.log(fetchData);
       } catch (error) {
         console.log("There has been a issue fetching the forecast");
@@ -18,22 +20,41 @@ export default function Forecast() {
     getForecast();
   }, []);
 
+  const convertToFehrenheit = (celsius) => {
+    const tempCelsius = celsius - 273.15;
+    const tempFahrenheit = (tempCelsius * 9) / 5 + 32;
+    return parseInt(tempFahrenheit);
+  }
+
   return (
     <div className="pt-[60px]">
       <div className>
-      <h1 className="text-2xl pb-4">Daily Forecast</h1>
-      <div className="flex justify-center gap-16 border-t-2 border-white mx-[500px]">
-        {forecast && forecast.length != 0 ? (
-          forecast.map((item) => (
-            <div>
-              <h3 className="text-xl">{item.dayOfWeek}</h3>
-            </div>
-          ))
-        ) : (
-          <p></p>
-        )}
+        <h1 className="text-2xl pb-4">Daily Forecast</h1>
+        <div className="flex justify-center gap-16 border-t-2 border-white mx-[450px]">
+          {forecast && forecast.length != 0 ? (
+            forecast.map((item) => (
+              <div className="pt-4">
+                <h3 className="text-3xl">{item.dayOfWeek}</h3>
+                <h4 className="text-lg">{item.date}</h4>
+                <div className="flex justify-center">
+                  <img
+                    src={`https://openweathermap.org/img/w/${item.icon}.png`}
+                    alt="weather icon"
+                  />
+                </div>
+                <div className="flex justify-center">
+                  <BiUpArrowAlt className="text-2xl"/>
+                  <p className="text-xl">{convertToFehrenheit(item.maxTemp)}&deg;F</p>
+                  <BiDownArrowAlt className="text-2xl"/>
+                  <p className="text-xl">{convertToFehrenheit(item.minTemp)}&deg;F</p>
+                </div>
+              </div>
+            ))
+          ) : (
+            <p></p>
+          )}
+        </div>
       </div>
-    </div>
     </div>
   );
 }
--- client/my-react-app/src/pages/HomePage.jsx
+++ client/my-react-app/src/pages/HomePage.jsx
@@ -22,7 +22,7 @@ export default function HomePage() {
 
   return (
     <div className='px-4 py-6 md:py-10 text-center text-3xl md:text-[50px] w-full text-white
-    bg-[radial-gradient(ellipse_at_left,_var(--tw-gradient-stops))] from-blue-100 via-blue-300 to-blue-500
+    bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-blue-700 via-blue-800 to-gray-900
     '>
         <h1>New York, NY</h1>
         <Weather/>
